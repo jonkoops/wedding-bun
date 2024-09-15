b08d03e66fec10f221bca0c1c02bbaeb9767a7c9
Jon Koops <jonkoops@gmail.com>
2024-09-15 19:48:25 +0200

    Update photos page with actual content
    Signed-off-by: Jon Koops <jonkoops@gmail.com>

--- environment.ts
+++ environment.ts
@@ -1,4 +1,4 @@
-const { DATABASE_URL, SESSION_SECRET, PASSCODE, NODE_ENV } = process.env;
+const { DATABASE_URL, SESSION_SECRET, PASSCODE, PHOTOS_URL, NODE_ENV } = process.env;
 
 // Check environment variables
 if (!DATABASE_URL) {
@@ -13,9 +13,14 @@ if (!PASSCODE) {
   throw new Error("The 'PASSCODE' environment variable must be set.");
 }
 
+if (!PHOTOS_URL) {
+  throw new Error("The 'PHOTOS_URL' environment variable must be set.");
+}
+
 export const environment = {
   databaseUrl: DATABASE_URL,
   sessionSecret: SESSION_SECRET,
   passcode: PASSCODE,
+  photosUrl: PHOTOS_URL,
   isProduction: NODE_ENV === "production",
 };
--- routes/photos.ts
+++ routes/photos.ts
@@ -1,7 +1,15 @@
 import { Router } from "express";
 import { codeRequired } from "../middleware/code-required";
+import { renderSVG } from "uqr";
+import { environment } from "../environment";
+
+const PHOTOS_URL_SVG = renderSVG(environment.photosUrl);
 
 export const photosRouter = Router();
 
 photosRouter.use(codeRequired);
-photosRouter.get("/", (req, res) => res.render("photos"));
+
+photosRouter.get("/", (req, res) => res.render("photos", {
+  photosUrl: environment.photosUrl,
+  photosUrlSvg: PHOTOS_URL_SVG
+}));
